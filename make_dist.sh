0cc7c3a1c2e8608523c6eab68b09ddc882c542f9
Jukka Turunen <ext-jukka.turunen@hiq.fi>
2017-06-21 23:28:35 +0200

    made distribution build scriot which copies only needed files to dist-folder

---
+++ make_dist.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+echo "Copy production files to ./dist directory"
+echo "----------"
+
+echo "cp ./index.html ./dist/"
+cp ./index.html ./dist/
+if [ $? -eq 0 ]
+then
+  echo "===>  OK"
+else
+  echo "==> FAILED"
+  exit -1
+fi
+
+d=./dist/assets/css/
+file=./assets/css/main.min.css
+echo "cp -R $file $d"
+test -d "$d" || mkdir -p "$d" && cp $file "$d"
+if [ $? -eq 0 ]
+then
+  echo "===>  OK"
+else
+  echo "==> FAILED"
+  exit -1
+fi
+
+d=./dist/assets/img/
+file=./assets/img/*
+echo "cp -R $file $d"
+test -d "$d" || mkdir -p "$d" && cp $file "$d"
+if [ $? -eq 0 ]
+then
+  echo "===>  OK"
+else
+  echo "==> FAILED"
+  exit -1
+fi
+
+d=./dist/assets/js/
+file=./assets/js/functions.min.js
+echo "cp -R $file $d"
+test -d "$d" || mkdir -p "$d" && cp $file "$d"
+if [ $? -eq 0 ]
+then
+  echo "===>  OK"
+else
+  echo "==> FAILED"
+  exit -1
+fi
+
+d=./dist/assets/js/vendor/
+file=./assets/js/vendor/jquery-2.2.4.min.js
+echo "cp -R $file $d"
+test -d "$d" || mkdir -p "$d" && cp $file "$d"
+if [ $? -eq 0 ]
+then
+  echo "===>  OK"
+else
+  echo "==> FAILED"
+  exit -1
+fi
+
+d=./dist/assets/videos/
+file=./assets/videos/HOTEP_A2_2.mp4
+echo "cp -R $file $d"
+test -d "$d" || mkdir -p "$d" && cp $file "$d"
+if [ $? -eq 0 ]
+then
+  echo "===>  OK"
+else
+  echo "==> FAILED"
+  exit -1
+fi
+
+exit 0
